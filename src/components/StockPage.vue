eb4d5b93a19352cf9c024cfe2453755de7330f27
chenhan20 <a0911558945@gmail.com>
2022-09-04 22:29:25 +0800

    小修改

--- src/components/StockPage.vue
+++ src/components/StockPage.vue
@@ -6,36 +6,40 @@ const stockData = data["data"];
 
 <template>
   <h1 class="text-xl font-mono"></h1>
-  <div class="flex justify-center">
-    <table class="">
-      <thead
-        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-      >
-      
-        <tr>
-          <th class="py-3 px-6" v-for="field in fields">
-            {{ field }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-          v-for="stock in stockData"
+  <div class="bg-cover bg-center bg-gray-700" style="">
+    <div class="flex justify-center">
+      <table class="table-auto">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
         >
-          <td
-            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            v-for="data in stock"
+          <tr>
+            <th class="py-3 px-6" v-for="field in fields">
+              {{ field }}
+            </th>
+          </tr>
+        </thead>
+        <tbody> 
+          <tr
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            v-for="stock in stockData"
           >
-            {{ data }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            <td
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              v-for="data in stock"
+            >
+              {{ data }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <style scoped>
+body {
+  background-color: #42b983;
+}
 a {
   color: #42b983;
 }
