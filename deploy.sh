71bc8bd633e55e45eadc4779e9c9c43871c68eaa
chenhan20 <a0911558945@gmail.com>
2022-12-03 20:33:47 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,31 @@
+# npm run build
+# git rm -r --cached dist
+# git subtree push --prefix dist origin gh-pages
+# git add dist
+# git commit -m 'update gh-pages'
+# git subtree push --prefix dist origin gh-pages
+
+
+# abort on errors
+set -e
+
+# build
 npm run build
-git rm -r --cached dist
-git subtree push --prefix dist origin gh-pages
-git add dist
-git commit -m 'update gh-pages'
-git subtree push --prefix dist origin gh-pages
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/chenhan20/vite-linkTree.gitt gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
