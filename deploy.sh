064cf6c0c4bc8be2e8d054135f4b6e3db2ed8202
chenhan20 <a0911558945@gmail.com>
2022-12-03 20:44:57 +0800

    deploy.sh更新

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/chenhan20/vite-linkTree.gitt gh-pages
+git push -f https://github.com/chenhan20/vite-linkTree.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
